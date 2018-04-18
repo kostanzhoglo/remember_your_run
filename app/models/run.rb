95058044096afd76cec9674855867a0a247198dc
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-18 16:21:53 -0400

    Build run.rb#format_pace_per_mile and have it display in previous runs

--- app/models/run.rb
+++ app/models/run.rb
@@ -3,7 +3,12 @@ class Run < ApplicationRecord
   belongs_to :month
 
   def format_pace_per_mile
-    10
+    seconds_per_mile = ((self.duration * 60) / self.distance)
+    seconds_per_mile.round
+    Time.at(seconds_per_mile).utc.strftime("%M:%S")
+
+    # seconds_remainder = (self.duration).divmod(self.distance)
+    # seconds_remainder.round(2)
 
   end
 
