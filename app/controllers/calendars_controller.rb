ef1f2655cad8c7cdbc2830179fd4c2871c308e01
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-18 06:51:55 -0400

    Add calendar#create, Add section class to home.html.erb new_calendar_form

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -4,4 +4,11 @@ class CalendarsController < ApplicationController
     @calendar = Calendar.find_by(id: params[:id])
   end
 
+  def create
+    @calendar = Calendar.new(params[:calendar])
+    @calendar.save
+
+    redirect_to calendar_url(@calendar)
+  end
+
 end
