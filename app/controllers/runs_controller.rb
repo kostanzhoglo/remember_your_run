e2e8c54936bdbcd5638931608b599b6cf188d104
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-18 15:32:02 -0400

    Complete runs#create, test how to format pace_per_mile in run.rb

--- app/controllers/runs_controller.rb
+++ app/controllers/runs_controller.rb
@@ -2,7 +2,10 @@ class RunsController < ApplicationController
 
 
   def create
-    raise params.inspect
+    @month = Month.find(params[:month_id])
+    @run = @month.runs.build(run_params)
+    @run.save
+    redirect_to month_path(@month)
   end
 
   def index
@@ -13,5 +16,12 @@ class RunsController < ApplicationController
     @month = Month.find_by(id: params[:id])
     @run = Run.find_by(id: params[:id])
   end
+
+
+  private
+
+    def run_params
+      params.require(:run).permit(:date, :name, :distance, :duration, :pace_per_mile, :notes, :number_intervals, :interval_length, :rest_between_interval)
+    end
 
 end
--- app/models/run.rb
+++ app/models/run.rb
@@ -2,4 +2,9 @@ class Run < ApplicationRecord
 
   belongs_to :month
 
+  def format_pace_per_mile
+    10
+
+  end
+
 end
