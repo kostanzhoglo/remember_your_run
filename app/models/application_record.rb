43fc01fc28d9ea9aa6e5f4d1ed5e00072153c767
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-21 14:13:35 -0400

    Move #total_seconds to application_controller from runs_controller, build Monthly pace and total mileage functions

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,18 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def total_seconds(entry)
+    time_array = entry.split(":").collect {|index| index.to_i}
+    if time_array.count == 2
+      total_seconds = (time_array[0] * 60) + time_array[1]
+    elsif time_array.count == 3
+      total_seconds = (time_array[0] * 3600) + (time_array[1] * 60) + time_array[2]
+    else
+      total_seconds = time_array[0]
+    end
+  end
+
+
+
+
 end
--- app/models/month.rb
+++ app/models/month.rb
@@ -8,4 +8,13 @@ class Month < ApplicationRecord
     self.runs.collect {|run| run.distance}.sum
   end
 
+  def month_total_run_time
+    self.runs.collect {|run| total_seconds(run.duration)}.sum
+  end
+
+  def month_pace
+    seconds_per_mile = (month_total_run_time / month_distance).round
+    Time.at(seconds_per_mile).utc.strftime("%_M:%S")
+  end
+
 end
