ace76e4b8e4be35c24e894d8c3e109aa749b207e
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-22 13:05:45 -0400

    Create User model and migration and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_18_220150) do
+ActiveRecord::Schema.define(version: 2018_04_22_170109) do
 
   create_table "months", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,12 @@ ActiveRecord::Schema.define(version: 2018_04_18_220150) do
     t.datetime "updated_at", null: false
     t.integer "month_id"
     t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
