94513bbecb04bd860b8dd294217f3e8b53611a8a
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-23 12:44:33 -0400

    Add password_confirmation to users/new.html.erb form, and validates password_confirmation to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true
   validates :email, uniqueness: true
+  validates :password, presence: true
+  validates :password, confirmation: true
 
   has_many :runs
   has_many :months, through: :runs
