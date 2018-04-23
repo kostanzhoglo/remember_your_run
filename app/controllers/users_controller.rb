4743f23ed182b942291bcab2af0f23032b53023c
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-23 09:20:34 -0400

    Edit user.rb validations, add users#create save? conditional code, add error_explanation to users/new.html.erb and CSS styling to same

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-    @user.save
-
-    redirect_to "/"
+    if @user.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
 
+  validates :email, presence: true
+  validates :email, uniqueness: true
+
   has_many :runs
   has_many :months, through: :runs
 
