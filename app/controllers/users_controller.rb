704d228d8f0ce8ae693f487ea560d2ade026f9d1
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-23 12:35:54 -0400

    Add :password field to signup/login pages, add password_logic to users/create and Authenticate user in sessions#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
@@ -17,7 +18,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:email)
+      params.require(:user).permit(:email, :password)
     end
 
 end
