3620115b9967bac379fbdfea0519e7411e27ee60
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-24 10:56:57 -0400

    Add github OAuth login branch in sessions#create (works), Add back in current_user line in months/index.html.erb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,30 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_path
+    # When they login via OAuth
+    if auth_hash = request.env["omniauth.auth"]
+      oauth_email = request.env["omniauth.auth"]["info"]["email"]
+      if user = User.find_by(email: oauth_email)
+        session[:user_id] = user.id
+        redirect_to root_path
+      else
+        user = User.create(email: oauth_email, password: SecureRandom.hex)
+        if user.save
+          session[:user_id] = user.id
+          redirect_to root_path
+        else
+          raise user.errors.full_messages.inspect
+        end
+      end
     else
-      render "sessions/new"
+    # Normal LOCAL login
+      user = User.find_by(email: params[:email])
+      if user && user.authenticate(params[:password])
+        session[:user_id] = user.id
+        redirect_to root_path
+      else
+        render "sessions/new"
+      end
     end
   end
 
