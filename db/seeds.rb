c045f6814bb367c01d3617fa2bf3b07bcf874eef
kostanzhoglo <jeffreywithers@gmail.com>
2018-04-27 07:55:47 -0400

    Add :pace_per_mile to seeds.rb data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,5 +14,5 @@ apr = Month.create(name: "April", year: "2018", goal: "Run 60 miles this month")
 
 
 # runs
-bobby1 = Run.create(date: "3/1", name: "Shore Dr.", distance: 4.1, duration: "35:00", notes: "Sunny outside", number_intervals: 10, interval_length: ":50", rest_between_interval: ":40", month_id: 1, user_id: 1)
-bobby2 = Run.create(date: "4/1", name: "Shore Dr.", distance: 4.1, duration: "33:00", notes: "Fast today", number_intervals: 15, interval_length: ":50", rest_between_interval: ":40", month_id: 2, user_id: 1)
+bobby1 = Run.create(date: "3/1", name: "Shore Dr.", distance: 4.1, duration: "35:00", pace_per_mile: "8:32", notes: "Sunny outside", number_intervals: 10, interval_length: ":50", rest_between_interval: ":40", month_id: 1, user_id: 1)
+bobby2 = Run.create(date: "4/1", name: "Shore Dr.", distance: 4.1, duration: "33:00", pace_per_mile: "8:03", notes: "Fast today", number_intervals: 15, interval_length: ":50", rest_between_interval: ":40", month_id: 2, user_id: 1)
