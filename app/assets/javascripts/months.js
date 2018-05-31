5ffdfe3af64437318139eeeab8b762a2afcf15e6
kostanzhoglo <jeffreywithers@gmail.com>
2018-05-31 15:28:30 -0400

    Middle of debug process for broken months links in months/index.html.erb

--- app/assets/javascripts/months.js
+++ app/assets/javascripts/months.js
@@ -1,10 +1,16 @@
+// $(function () {
+//   $(".js-month-info").on('click', function() {
+//     var id = $(this).data("id");
+//     $.get("/months/" + id + "/info", function(data) {
+//       $("#avg_pace-" + id).html(data);
+//     });
+//   });
+// });
+
+
 $(function () {
-
-  $(".js-month-info").on('click', function() {
-    var id = $(this).data("id");
-    $.get("/months/" + id + "/info", function(data) {
-      $("#avg_pace-" + id).text(data);
-    });
+  $(".js-month-info").on('click', function(e) {
+    alert("you clicked this link")
+    e.preventDefault();
   });
-
-});
+})
