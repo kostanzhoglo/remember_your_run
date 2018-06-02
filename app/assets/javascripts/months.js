65ad116b1fd4a96035e633afbb98090902448925
kostanzhoglo <jeffreywithers@gmail.com>
2018-06-01 21:56:23 -0400

    Totally stuck on how to display data from my nested JSON file. Am looking at runs: [object Object] [object Object]. Working on it

--- app/assets/javascripts/months.js
+++ app/assets/javascripts/months.js
@@ -1,13 +1,22 @@
 $(function () {
+
   $(".js-month-info").on('click', function() {
     var id = $(this).data("id");
+
     $.get("/months/" + id + ".json", function(data) {
       // $(".avg_pace-" + id).html(data);
       // $(".month_name-" + id).text(data["name"]);
-      $(".month_goal-" + id).text(data["goal"]);
-      $(".month_run_name" + id).text(data["run"]["name"]);
+      $(".month_goal-" + id).html(data["goal"]);
+      $.each(data, function(key, value) {
+        console.log(key + " : " + value)
+      });
+
+
+      // $(".month_run_name" + id).text(data["run"]["name"]);
     });
+
   });
+
 });
 
 
