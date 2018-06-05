c10bdf86a29eef30d61bf872b17a0b335d47594d
kostanzhoglo <jeffreywithers@gmail.com>
2018-06-05 14:03:55 -0400

    Left comment to ask Cernan about HTML error for tomorrow. Fix months.js continues

--- app/assets/javascripts/months.js
+++ app/assets/javascripts/months.js
@@ -1,27 +1,29 @@
-// $(function () {
-//
-//   $(".js-month-info").on('click', function() {
-//     var id = $(this).data("id");
-//
-//     $.get("/months/" + id + ".json", function(data) {
-//       // $(".avg_pace-" + id).html(data);
-//       // $(".month_name-" + id).text(data["name"]);
-//       $(".month_goal-" + id).html(data["goal"]);
-//       let output = "";
-//       $.each(data["runs"], function(key, value) {
-//         // output += " " + data["goal"]
-//
-//         $("#run_distance-" + id).append((key + 1) + " distance: " + value.distance + "<br>")
-//         $("#run_duration-" + id).append((key + 1) + " time: " + value.duration + "<br>")
-//         $("#run_date-" + id).append((key + 1) + " date:" + value.date + "<br>")
-//         $("#run_name-" + id).append((key + 1) + " name:" + value.name + "<br>")
-//         $("#run_container-" + id).append((key + 1) + " pace_per_mile:" + value.pace_per_mile + "<br>")
-//
-//       });
-//
-//     });
-//
-//   });
+$(function () {
+
+  $(".js-month-info").on('click', function() {
+    var id = $(this).data("id");
+
+    $.get(`/months/${id}.json`, function(data) {
+
+      // fetch(`/months/${id}.json`)
+      //   .then(res => res.json())
+      //   .then(data => (data))
+
+      $(".month_goal-" + id).html(data["goal"]);
+
+      $.each(data["runs"], function(key, value) {
+
+        $("#run_distance-" + id).append((key + 1) + " distance: " + value.distance + "<br>")
+        $("#run_duration-" + id).append((key + 1) + " time: " + value.duration + "<br>")
+        $("#run_date-" + id).append((key + 1) + " date:" + value.date + "<br>")
+        $("#run_name-" + id).append((key + 1) + " name:" + value.name + "<br>")
+        $("#run_container-" + id).append((key + 1) + " pace_per_mile:" + value.pace_per_mile + "<br>")
+
+      });
+
+    });
+
+  });
 
   // function Run(date, name, distance, duration, pace_per_mile) {
   //   this.date = date
@@ -31,19 +33,31 @@
   //   this.pace_per_mile = pace_per_mile
   // }
 
-// });
+});
 
-$(() => {
-  bindClickHandlers()
-})
 
-const bindClickHandlers = () => {
-  $(".js-month-info").on('click', (e) => {
-    e.preventDefault();
-    var id = $(this).data("id")
-    $.get("/months/" + id + ".json", function(data) {
+
+
+// Something kept breaking where instead of rendering JSON, the functions
+//   below were trying to render my application.html.erb page.
+// THOUGHTS??
+
+// $(() => {
+//   bindClickHandlers()
+// })
+//
+// const bindClickHandlers = () => {
+//   $(".js-month-info").on('click', (e) => {
+//     var id = $(this).data("id")
+//      $.get("/months/" + id + ".json", function(data) {
+
+
     // fetch(`/months/${id}.json`)
     //   .then(res => res.json())
     //   .then(data => (data))
-  })
-}
+
+
+//     console.log(data)
+//     })
+// })
+// }
