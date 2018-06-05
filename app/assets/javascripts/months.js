8f9c854154a75b289348e600f3b656ae6f09d1be
kostanzhoglo <jeffreywithers@gmail.com>
2018-06-05 14:26:20 -0400

    Refactor run object constructor function in months.js

--- app/assets/javascripts/months.js
+++ app/assets/javascripts/months.js
@@ -8,34 +8,37 @@ $(function () {
       // fetch(`/months/${id}.json`)
       //   .then(res => res.json())
       //   .then(data => (data))
-
+      $('#run_container-' + id).html('')
       $(".month_goal-" + id).html(data["goal"]);
 
-      $.each(data["runs"], function(key, value) {
+      // $.each(data["runs"], function(key, value) {
+      data["runs"].forEach(run => {
+        console.log(run)
+      // })
 
-        $("#run_distance-" + id).append((key + 1) + " distance: " + value.distance + "<br>")
-        $("#run_duration-" + id).append((key + 1) + " time: " + value.duration + "<br>")
-        $("#run_date-" + id).append((key + 1) + " date:" + value.date + "<br>")
-        $("#run_name-" + id).append((key + 1) + " name:" + value.name + "<br>")
-        $("#run_container-" + id).append((key + 1) + " pace_per_mile:" + value.pace_per_mile + "<br>")
+        // $("#run_distance-" + id).append((key + 1) + " distance: " + value.distance + "<br>")
+        // $("#run_duration-" + id).append((key + 1) + " time: " + value.duration + "<br>")
+        // $("#run_date-" + id).append((key + 1) + " date:" + value.date + "<br>")
+        // $("#run_name-" + id).append((key + 1) + " name:" + value.name + "<br>")
+        // $("#run_container-" + id).append((key + 1) + " pace_per_mile:" + value.pace_per_mile + "<br>")
 
       });
-
     });
-
   });
-
-  // function Run(date, name, distance, duration, pace_per_mile) {
-  //   this.date = date
-  //   this.name = name
-  //   this.distance = distance
-  //   this.duration = duration
-  //   this.pace_per_mile = pace_per_mile
-  // }
-
 });
 
-
+function Run(run) {
+  this.id = run.id
+  this.date = run.date
+  this.name = run.name
+  this.distance = run.distance
+  this.duration = run.duration
+  this.pace_per_mile = run.pace_per_mile
+  this.number_intervals = run.number_intervals
+  this.interval_length = run.interval_length
+  this.rest_between_interval = run.rest_between_interval
+  this.notes = run.notes
+}
 
 
 // Something kept breaking where instead of rendering JSON, the functions
