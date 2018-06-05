eae9ddc82ba1e8adc6f0e081e9c97b7a3d675817
kostanzhoglo <jeffreywithers@gmail.com>
2018-06-04 20:57:50 -0400

    Fix display issues with runs/show.html.erb and cycling thru Next button

--- app/assets/javascripts/runs.js
+++ app/assets/javascripts/runs.js
@@ -2,18 +2,19 @@
 $(function () {
   $(".js-next-run").on("click", function() {
     var nextId = parseInt($(".js-next-run").attr("data-id")) + 1;
-    console.log(nextId)
-    $.get("/runs/" + nextId + ".json", function(data) {
+    // console.log(nextId)
+    $.get("/runs/" + nextId + ".json", function(run) {
       // var run = data;
-      $("#runDate").text(data["date"]);
-      $("#runName").text(data["name"]);
-      $(".runDistance").text(data["distance"]);
-      // $(".runPacePerMile").text(run["pace_per_mile"]);
-      // $(".runNumberIntervals").text(run["number_intervals"]);
-      // $(".runIntervalLength").text(run["interval_length"]);
-      // $(".runRestBetween").text(run["rest_between_interval"]);
+      $("#runDate").text(run["date"]);
+      $("#runName").text(run["name"]);
+      $("#runDistance").text(run["distance"]);
+      $("#runDuration").text(run["duration"]);
+      $("#runPacePerMile").text(run["pace_per_mile"]);
+      $("#runNumberIntervals").text(run["number_intervals"]);
+      $("#runIntervalLength").text(run["interval_length"]);
+      $("#runRestBetween").text(run["rest_between_interval"]);
       // re-set the id to current on the link
-      $(".js-next-run").attr("data-id", data["id"]);
+      $(".js-next-run").attr("data-id", run["id"]);
     });
   });
 
