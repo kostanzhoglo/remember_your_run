11845cfa072adc392ace2d178d164780dda131bf
kostanzhoglo <jeffreywithers@gmail.com>
2018-06-05 12:56:35 -0400

    More refactoring of months.js

--- app/assets/javascripts/months.js
+++ app/assets/javascripts/months.js
@@ -1,27 +1,27 @@
-$(function () {
-
-  $(".js-month-info").on('click', function() {
-    var id = $(this).data("id");
-
-    $.get("/months/" + id + ".json", function(data) {
-      // $(".avg_pace-" + id).html(data);
-      // $(".month_name-" + id).text(data["name"]);
-      $(".month_goal-" + id).html(data["goal"]);
-      let output = "";
-      $.each(data["runs"], function(key, value) {
-        // output += " " + data["goal"]
-
-        $("#run_distance-" + id).append((key + 1) + " distance: " + value.distance + "<br>")
-        $("#run_duration-" + id).append((key + 1) + " time: " + value.duration + "<br>")
-        $("#run_date-" + id).append((key + 1) + " date:" + value.date + "<br>")
-        $("#run_name-" + id).append((key + 1) + " name:" + value.name + "<br>")
-        $("#run_container-" + id).append((key + 1) + " pace_per_mile:" + value.pace_per_mile + "<br>")
-
-      });
-
-    });
-
-  });
+// $(function () {
+//
+//   $(".js-month-info").on('click', function() {
+//     var id = $(this).data("id");
+//
+//     $.get("/months/" + id + ".json", function(data) {
+//       // $(".avg_pace-" + id).html(data);
+//       // $(".month_name-" + id).text(data["name"]);
+//       $(".month_goal-" + id).html(data["goal"]);
+//       let output = "";
+//       $.each(data["runs"], function(key, value) {
+//         // output += " " + data["goal"]
+//
+//         $("#run_distance-" + id).append((key + 1) + " distance: " + value.distance + "<br>")
+//         $("#run_duration-" + id).append((key + 1) + " time: " + value.duration + "<br>")
+//         $("#run_date-" + id).append((key + 1) + " date:" + value.date + "<br>")
+//         $("#run_name-" + id).append((key + 1) + " name:" + value.name + "<br>")
+//         $("#run_container-" + id).append((key + 1) + " pace_per_mile:" + value.pace_per_mile + "<br>")
+//
+//       });
+//
+//     });
+//
+//   });
 
   function Run(date, name, distance, duration, pace_per_mile) {
     this.date = date
@@ -32,6 +32,17 @@ $(function () {
   }
 
 });
+
+$(() => {
+  bindClickHandlers()
+})
+
+const bindClickHandlers = () => {
+  $(".js-month-info").on('click', (e) => {
+    e.preventDefault();
+
+  })
+}
 
 
 // $(function () {
