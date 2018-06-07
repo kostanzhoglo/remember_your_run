dd53751253dc76dad91a13e36b3394a888116090
kostanzhoglo <jeffreywithers@gmail.com>
2018-06-07 17:40:49 -0400

    Add class to create new month Form, specify new form class in click event handler in months.js

--- app/assets/javascripts/months.js
+++ app/assets/javascripts/months.js
@@ -22,7 +22,7 @@ $(function () {
 });
 
 $(function () {
-  $('form').submit(function(event) {
+  $('month_form').submit(function(event) {
     //prevent form from submitting the default way
     event.preventDefault();
     alert("we r hack3rz");
